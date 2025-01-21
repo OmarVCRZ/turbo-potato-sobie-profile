6ccb51687638e0d3f18cde391fc35d4f5d0dc7b9
Diaz Martinez <odiazmartinez@una.edu>
2025-01-21 17:05:51 -0600

    Added

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
 
+// Code Changed
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
 
 app.get('/ejs', (req, res) => {
-  res.render('words',
+  res.render('word',
     {pageTitle: 'my cools ejs page'}
   );
 })
+
 app.get('/nodemon', (req, res) => {
   res.sendFile('no kill node process then restart and fresh')
 })
@@ -23,6 +25,7 @@ app.get('/nodemon', (req, res) => {
 app.get('/helloRender', (req, res) => {
   res.send('Hello Express from Real World!<br><a href="/">Back to Home</>')
 })
+
 
 app.listen(
   port,
