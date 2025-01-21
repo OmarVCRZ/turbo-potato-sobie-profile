99d9518fe853bece2d5a2ddef2b956e9859e72bc
Diaz Martinez <odiazmartinez@una.edu>
2025-01-21 16:52:39 -0600

    Added things

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
+
+app.get('/ejs', (req, res) => {
+  res.render('words',
+    {pageTitle: 'my cools ejs page'}
+  );
+})
+app.get('/nodemon', (req, res) => {
+  res.sendFile('no kill node process then restart and fresh')
+})
 
 // endpoint, middleware(s)
-app.get('/', (req, res) => {
-  res.send('Hello Express!')
+app.get('/helloRender', (req, res) => {
+  res.send('Hello Express from Real World!<br><a href="/">Back to Home</>')
 })
 
 app.listen(
