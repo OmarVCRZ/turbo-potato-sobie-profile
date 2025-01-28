2b12e7a9cb5096fbd53dcaa1dab7f2c7d56753d1
Diaz Martinez <odiazmartinez@una.edu>
2025-01-28 16:46:40 -0600

    Added new stuff need updated app.js to be fixed

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;  
 const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); 
 app.use(express.static(__dirname + '/public'))
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI;
+
+// console.log(uri);
+
+// begin all my...
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // change my code 
 
@@ -19,13 +51,17 @@ app.post('/saveMyName', (req,res)=>{
 
   console.log(req.body); 
 
+
   // res.redirect('/ejs'); 
 
   res.render('words',
-    {pageTitle: req.body.myName});
+  {pageTitle: req.body.myName});
+
 
   // res.render('words',
-  //   {theData: req.body})
+  // {theData : req.body});
+
+
 })
 
 app.get('/saveMyNameGet', (req,res)=>{
@@ -37,11 +73,13 @@ app.get('/saveMyNameGet', (req,res)=>{
 
 })
 
+
 app.get('/ejs', function (req, res) {
   res.render('words',
     {pageTitle: 'my cool ejs page'}
   );
 })
+
 
 app.get('/nodemon', function (req, res) {
   res.send('look ma, no kill node process then restart node then refresh browser...cool?');
@@ -53,9 +91,12 @@ app.get('/helloRender', function (req, res) {
   res.send('Hello Express from Real World<br><a href="/">back to home</a>')
 })
 
+
+
+
 app.listen(
   port, 
   ()=> console.log(
-    `server is running on ... ${port}`
+    `server is running on ... localhost:${port}`
     )
   );
